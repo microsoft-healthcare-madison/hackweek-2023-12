745811a2f41a90e96de718cb6f7bbcf7f1849837
Josh Mandel <jmandel@alum.mit.edu>
2023-12-05 13:49:58 -0600

    Fix regex application

--- day-2-answer-suggestion/index.ts
+++ day-2-answer-suggestion/index.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { createFactModelForQuestion, identifyKeywordsForQuestion } from "./prompts";
+import { Keywords } from "./types";
 dotenv.config();
 
 interface Questionnaire {
@@ -21,9 +22,27 @@ function extractItemFromQuestionnaire(q: Questionnaire, linkId: string) {
   return result;
 }
 
+function applyRegex(expression: Keywords | string, input: string): boolean {
+  if (typeof expression === 'string') {
+    const regex = new RegExp(expression, 'gim');
+    return regex.test(input);
+  }
+
+  if ("or" in expression) {
+    return expression.or.some((exp: any) => applyRegex(exp, input));
+  }
+
+  if ("and" in expression) {
+    return expression.and.every((exp: any) => applyRegex(exp, input));
+  }
+
+  return false;
+}
+
 // debug with a random medical quesiton
 const debug = await identifyKeywordsForQuestion({text: "what stage was the breast cancer at the time of diagnosis?"})
 console.log(debug)
 
 const debug2 = await createFactModelForQuestion({text: "what stage was the breast cancer at time of diagnosis?"})
 console.log(debug2)
+
